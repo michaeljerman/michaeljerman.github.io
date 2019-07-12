8976c295c40af6103804cfb33fdea748a77aa562
mj <michael.jerman@gmail.com>
2019-07-12 14:51:55 -0700

    www copy

---
+++ teaching/311/js/magnific-popup-options.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+   // MagnificPopup
+	var magnifPopup = function() {
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery:{
+				enabled:true
+			}
+		});
+	};
+
+	var magnifVideo = function() {
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+	};
+
+    var magnifPDF = function() {
+	$('.popup-pdf').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+	});
+    };
+
+	
+
+
+	// Call the functions 
+	magnifPopup();
+    magnifVideo();
+    magnifPDF();
+
+});
